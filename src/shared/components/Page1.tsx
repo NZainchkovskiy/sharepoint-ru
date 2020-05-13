3d5f7d03089a52f603937731a29d0febe5b77938
Nikolay Zainchkovskiy <extAvara@aoavant.ru>
2020-05-13 16:14:22 +0300

    Changed lookup to real filter

--- src/shared/components/Page1.tsx
+++ src/shared/components/Page1.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { getListItems } from "../services/data-service";
+import { getListItems, getListItemsByParent } from "../services/data-service";
 import useSWR from "swr";
 import { ItemsList } from "./ItemsList";
 import { IListItem } from "../model/IListItem";
@@ -9,18 +9,19 @@ interface IPage1Props {}
 
 export const Page1: React.FC<IPage1Props> = (props) => {
   const [selectedItem, setSelectedItem] = React.useState<IListItem>(null);
+  const parentId = selectedItem ? selectedItem.ID : null;
   const { data: list1Data } = useSWR("List1", getListItems, {
     refreshInterval: 1000,
   });
-  const { data: list2Data } = useSWR("List2", getListItems, {
-    refreshInterval: 1000,
-  });
+  const { data: list2Data } = useSWR(
+    ["List2", parentId],
+    getListItemsByParent,
+    {
+      refreshInterval: 1000,
+    }
+  );
   const list1Items = list1Data || [];
-
   const list2Items = list2Data || [];
-  const list2FilteredItems = selectedItem
-    ? list2Items.filter((item) => item.ParentId === selectedItem.ID)
-    : list2Items;
 
   return (
     <>
@@ -33,7 +34,7 @@ export const Page1: React.FC<IPage1Props> = (props) => {
       <ItemsList items={list1Items} onItemSelected={setSelectedItem} />
 
       <Text variant="large">List2 (filtered)</Text>
-      <ItemsList items={list2FilteredItems} />
+      <ItemsList items={list2Items} />
     </>
   );
 };
--- src/shared/services/data-service.ts
+++ src/shared/services/data-service.ts
@@ -2,3 +2,9 @@ import { sp } from "@pnp/sp/presets/all";
 import { IListItem } from "../model/IListItem";
 export const getListItems = (listTitle: string) =>
   sp.web.lists.getByTitle(listTitle).items.get<IListItem[]>();
+
+export const getListItemsByParent = (listTitle: string, parentId: number) =>
+  sp.web.lists
+    .getByTitle(listTitle)
+    .items.filter(`ParentId eq ${parentId}`)
+    .get<IListItem[]>();
